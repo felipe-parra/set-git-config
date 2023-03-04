dd3bfc29a762aa808d03970bed414a13b012c573
Felipe Parra <fpr29.10@gmail.com>
2023-03-04 14:33:48 -0600

    Add error handling to askForGitConfig() function

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,20 @@ import inquirer from "inquirer";
 import { exec } from "child_process";
 
 export async function main() {
-  const res = await askForGitConfig();
-  if (res.type === "Global") {
-    await runCommand(`git config --global user.name "${res.name}"`);
-    await runCommand(`git config --global user.email "${res.email}"`);
-  } else {
-    await runCommand(`git config --local user.name "${res.name}"`);
-    await runCommand(`git config --local user.email "${res.email}"`);
+  try {
+    const res = await askForGitConfig();
+    if (res.type === "Global") {
+      await runCommand(`git config --global user.name "${res.name}"`);
+      await runCommand(`git config --global user.email "${res.email}"`);
+    } else {
+      await runCommand(`git config --local user.name "${res.name}"`);
+      await runCommand(`git config --local user.email "${res.email}"`);
+    }
+    console.log("Git configuration set!");
+    console.log("Your name is: ", res.name, " and your email is: ", res.email);
+  } catch (err) {
+    console.log("Error", err);
   }
-  console.log("Git configuration set!");
-  console.log("Your name is: ", res.name, " and your email is: ", res.email);
 }
 
 // runCommand executes a shell command and returns the output as a string.
@@ -42,7 +46,6 @@ function runCommand(command: string): Promise<string> {
 // Side effects: none
 // Notes: none
 async function askForGitConfig() {
-  console.log("Set your git configuration: ");
   const responses = await inquirer.prompt([
     {
       type: "input",
